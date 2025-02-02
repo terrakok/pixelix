a8be2f9dbb7f26218bbc761a133d42d0931ccdcf
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2025-02-02 21:01:47 +0100

    Use spaces instead of commas for digit grouping

--- app/src/commonMain/kotlin/com/daniebeler/pfpixelix/utils/StringFormat.kt
+++ app/src/commonMain/kotlin/com/daniebeler/pfpixelix/utils/StringFormat.kt
@@ -2,5 +2,5 @@ package com.daniebeler.pfpixelix.utils
 
 object StringFormat {
     fun groupDigits(number: Int?): String =
-        (number ?: 0).toString().reversed().chunked(3).joinToString(",").reversed()
+        (number ?: 0).toString().reversed().chunked(3).joinToString(" ").reversed()
 }
