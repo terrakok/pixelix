75b21c6107aab3aae42feeffea9f1670ffdac4b3
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2026-09-04 11:17:58 +0200

    Enable desktop smooth resize

--- desktopApp/src/main/kotlin/main.kt
+++ desktopApp/src/main/kotlin/main.kt
@@ -1,5 +1,7 @@
 import com.daniebeler.pfpixelix.desktopApp
 
 fun main(args: Array<String>) {
+    System.setProperty("skiko.rendering.macos.metalSynchronousLiveResize", "true")
+    System.setProperty("skiko.rendering.windows.direct3DSynchronousLiveResize", "true")
     desktopApp(args)
 }
